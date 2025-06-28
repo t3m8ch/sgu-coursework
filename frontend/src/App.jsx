7a92e1806b4d09baac037b8bd0786f5d9c4f162f
Artem Kudyakov <t3m8ch@yandex.ru>
2025-06-29 01:05:59 +0400

    feat: Send events from frontend to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import { useState, useEffect, useRef } from "react";
 
-const element = (node) => {
+const element = (node, bus) => {
   if (node.name === "fragment") {
-    return <>{node.children.map((child) => element(child))}</>;
+    return <>{node.children.map((child) => element(child, bus))}</>;
   }
 
   if (node.name === "row") {
     return (
-      <div className="row">{node.children.map((child) => element(child))}</div>
+      <div className="row">
+        {node.children.map((child) => element(child, bus))}
+      </div>
     );
   }
 
@@ -18,7 +20,17 @@ const element = (node) => {
   }
 
   if (node.name === "button") {
-    return <button>{node.children.map((child) => element(child))}</button>;
+    return (
+      <button
+        onClick={() => {
+          if (node.props.on_click_event) {
+            bus.sendEvent(node.props.on_click_event);
+          }
+        }}
+      >
+        {node.children.map((child) => element(child, bus))}
+      </button>
+    );
   }
 
   return <></>;
@@ -28,6 +40,25 @@ function App() {
   const [uiTree, setUiTree] = useState(null);
   const [wsStatus, setWsStatus] = useState("disconnected");
   const socketRef = useRef(null);
+
+  const bus = {
+    sendEvent: (event) => {
+      if (
+        socketRef.current &&
+        socketRef.current.readyState === WebSocket.OPEN
+      ) {
+        socketRef.current.send(
+          JSON.stringify({
+            Event: {
+              plugin_name: "simple-plugin",
+              event,
+              data: {},
+            },
+          }),
+        );
+      }
+    },
+  };
 
   useEffect(() => {
     const socket = new WebSocket("ws://localhost:8000/ws");
@@ -59,7 +90,7 @@ function App() {
   return (
     <div>
       <div>{wsStatus}</div>
-      <div>{uiTree === null ? "Loading..." : element(uiTree)}</div>
+      <div>{uiTree === null ? "Loading..." : element(uiTree, bus)}</div>
     </div>
   );
 }
