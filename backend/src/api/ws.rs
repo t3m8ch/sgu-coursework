08f63de60f646ec5876be61c9da1e9a096f778ad
Artem Kudyakov <t3m8ch@yandex.ru>
2025-06-30 03:26:58 +0400

    fix: Fix websocket handler

--- backend/src/api/ws.rs
+++ backend/src/api/ws.rs
@@ -31,6 +31,7 @@ pub async fn ws(
         while let Some(Ok(msg)) = msg_stream.recv().await {
             match msg {
                 Message::Text(msg) => {
+                    log::info!("Received message: {}", msg);
                     if let Ok(action) = serde_json::from_str::<ActionReq>(&msg.to_string()) {
                         if let Err(err) = handle_action(action, &mut session, &app_state).await {
                             log::error!("Handle action error: {:#?}", err);
@@ -53,7 +54,7 @@ async fn handle_action(
     ws_session: &mut actix_ws::Session,
     app_state: &AppState,
 ) -> anyhow::Result<()> {
-    log::info!("Mounting {}", action_req.plugin_name);
+    log::info!("Action {:#?}", action_req);
     let plugin = app_state
         .plugins
         .lock()
@@ -65,24 +66,54 @@ async fn handle_action(
     let not_found = ActionRes::Error("Plugin not found".to_string());
     match plugin {
         Some(mut plugin) => {
-            let session_state = plugin.state(StateInput {
-                action: action_req.action,
-                old_state: None,
-            })?;
-            let session_id = uuid::Uuid::new_v4();
+            let ui_state = match action_req.action {
+                Action::Mount => {
+                    let session_id = uuid::Uuid::new_v4();
+                    let session = ActionRes::Session(session_id);
+                    ws_session.text(serde_json::to_string(&session)?).await?;
 
-            let init_state = serde_json::to_value(session_state)?;
-            app_state
-                .sessions
-                .lock()
-                .unwrap()
-                .insert(session_id, init_state.clone());
+                    let init_state = plugin.state(StateInput {
+                        action: action_req.action,
+                        old_state: None,
+                    })?;
 
-            let ui = ActionRes::UITree(plugin.ui(init_state)?);
+                    log::info!("Init state: {:#?}", init_state);
+
+                    app_state
+                        .sessions
+                        .lock()
+                        .unwrap()
+                        .insert(session_id, init_state.clone());
+
+                    serde_json::to_value(init_state)
+                }
+                Action::Event { event: _, data: _ } => {
+                    if let Some(session_id) = action_req.session_id {
+                        let old_state =
+                            app_state.sessions.lock().unwrap().get(&session_id).cloned();
+                        log::info!("Old state: {:#?}", old_state);
+
+                        let new_state = plugin.state(StateInput {
+                            action: action_req.action,
+                            old_state: old_state,
+                        })?;
+                        log::info!("New state: {:#?}", new_state);
+
+                        app_state
+                            .sessions
+                            .lock()
+                            .unwrap()
+                            .insert(session_id, new_state.clone());
+
+                        serde_json::to_value(new_state)
+                    } else {
+                        serde_json::to_value(ActionRes::Error("No session ID provided".to_string()))
+                    }
+                }
+            };
+
+            let ui = ActionRes::UITree(plugin.ui(ui_state?)?);
             ws_session.text(serde_json::to_string(&ui)?).await?;
-
-            let session = ActionRes::Session(session_id);
-            ws_session.text(serde_json::to_string(&session)?).await?;
         }
         None => ws_session.text(serde_json::to_string(&not_found)?).await?,
     };
